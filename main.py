b6b094e14a3c028ce595b839ab373311f6e973fe
vonrimS <mr.mytab@gmail.com>
2022-04-28 11:06:35 +0200

    base routing draft

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from flask.views import MethodView
 from wtforms import Form
+from flask import Flask
+
+
+app = Flask(__name__)
 
 class HomePage(MethodView):
-    pass
+    def get(self):
+        return 'Hello'
 
 class BillFormPage(MethodView):
-    pass
+    def get(self):
+        return 'I am the bill form page!'
 
 class ResultsPage(MethodView):
     pass
 
 class BillForm(Form):
     pass
+
+
+#routing
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
+
+app.run()
